ace3edfbe2712bde37fcc4b2998604af081fe366
wasiqafnan <wansari964@yahoo.com>
2025-04-18 13:58:47 +0530

    Added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState } from 'react';
+
+
+import './App.css';
+import Myapp from './components/MyApp';
+
+function App() {
+
+  return (
+    <>
+      <Myapp />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/MyApp.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+export default function MyApp() {
+    return (
+        <>
+            <div className="bg-red-500 text-white text-center p-4 rounded-md">
+                ✅ Tailwind is Working!
+            </div>
+        </>
+    )
+}
