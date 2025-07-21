6e6be0f85a6c81d6039493e9dd49549e66fc3b1a
wasiqafnan <wansari964@yahoo.com>
2025-07-22 00:15:33 +0530

    cookie update

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -104,6 +104,7 @@ const loginUser = async (req, res, next) => {
             secure: true,
             sameSite: "None",
             maxAge: 24 * 60 * 60 * 1000, // 1 day
+            path: "/",
         });
 
         // send response
