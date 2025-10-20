a30b3deb461b7b104416b8e0881bbeac0e134495
WasiqAfnan <wansari964@yahoo.com>
2025-10-20 13:01:19 +0530

    Add Db-ping route

--- backend/src/controllers/healthCheckController.js
+++ backend/src/controllers/healthCheckController.js
@@ -1,7 +1,21 @@
+import { ApiError, ApiResponse } from "../utils/index.js";
+import mongoose from "mongoose";
+
 export const healthCheckController = (req, res) => {
     res.json({
         status: 200,
         message:
             "Test API is working. Go to /api/user/signup to signup or /api/user/login to login",
     });
+};
+
+export const dbPingController = async (req, res) => {
+    try {
+        const resp = await mongoose.connection.db.admin().ping();
+        return res
+            .status(200)
+            .json(new ApiResponse("DB is up and running", 200, resp));
+    } catch (err) {
+        new ApiError("DB is down", 500);
+    }
 };
--- backend/src/routes/healthCheckRoutes.js
+++ backend/src/routes/healthCheckRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
 const healthCheckRouter = express.Router();
-import { healthCheckController } from "../controllers/healthCheckController.js";
+import {
+    dbPingController,
+    healthCheckController,
+} from "../controllers/healthCheckController.js";
 
 healthCheckRouter.route("/").get(healthCheckController);
+healthCheckRouter.route("/db-ping").get(dbPingController);
 
 export default healthCheckRouter;
